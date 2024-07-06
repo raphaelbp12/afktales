f756c425e32f9569693011412e3017c686a719a7
Raphael Parreira <raphaelbp12@gmail.com>
2024-07-06 10:01:18 +0200

    feat: update unity game

---
+++ changelogs/240625.ts
@@ -0,0 +1,15 @@
+import { ChangelogProps } from "@/components/commonComponents/Changelog";
+
+const changelog250624: ChangelogProps = {
+  date: "Junho 25, 2024",
+  changes: [
+    {
+      subtitle: "Mudanças no simulador da Tormenta dos Deuses",
+      items: [
+        "modificador de velocidade de movimento adicionado",
+      ],
+    },
+  ],
+};
+
+export default changelog250624;
---
+++ changelogs/240705.ts
@@ -0,0 +1,19 @@
+import { ChangelogProps } from "@/components/commonComponents/Changelog";
+
+const changelog240705: ChangelogProps = {
+  date: "Julho 05, 2024",
+  changes: [
+    {
+      subtitle: "Mudanças no simulador da Tormenta dos Deuses",
+      items: [
+        "jogador começa na entrada",
+        "a interface do usuario escala com a resolução da tela",
+        "botões para a seleção de fase do boss adicionados",
+        "o mostrador de cast do boss foi adicionado",
+        "fases 1 e 2 do boss foram aprimoradas",
+      ],
+    },
+  ],
+};
+
+export default changelog240705;
